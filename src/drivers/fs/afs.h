6b731c672e2e546ba8514a07ef48033897abd2fd
Gorciu <gorciuyt@gmail.com>
2024-06-16 12:16:47 +0200

    The AurorOS filesystem basic driver

--- src/drivers/fs/afs.h
+++ src/drivers/fs/afs.h
@@ -3,5 +3,86 @@
 // ======================================================================
 
 // => The AurorOS filesystem driver.
-//       This file is basically a inplementation of AurorOS filesystem
-//       and provides API calls to read the files, etc.
+//    This file is basically an implementation of the AurorOS filesystem
+//    and provides API calls to read files, etc.
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct afs_inode {
+    uint32_t inode_number;
+    uint32_t size;
+    uint64_t created_timestamp;
+    uint64_t modified_timestamp;
+};
+
+struct afs_file {
+    struct afs_inode inode;
+    uint8_t *data;
+};
+
+struct afs_directory {
+    struct afs_inode inode;
+    struct afs_inode *children;
+    size_t num_children;
+};
+
+// Functions should start with afs_
+
+void afs_init() {
+    // Initialization code here
+}
+
+int afs_create_file(const char *filename) {
+    // Create file implementation
+    return 0;
+}
+
+int afs_delete_file(const char *filename) {
+    // Delete file implementation
+    return 0;
+}
+
+struct afs_file *afs_open_file(const char *filename) {
+    // Open file implementation
+    return NULL;
+}
+
+int afs_read_file(struct afs_file *file, void *buffer, size_t count) {
+    // Read file implementation
+    return 0;
+}
+
+int afs_write_file(struct afs_file *file, const void *buffer, size_t count) {
+    // Write file implementation
+    return 0;
+}
+
+void afs_close_file(struct afs_file *file) {
+    // Close file implementation
+}
+
+int afs_create_directory(const char *dirname) {
+    // Create directory implementation
+    return 0;
+}
+
+int afs_delete_directory(const char *dirname) {
+    // Delete directory implementation
+    return 0;
+}
+
+struct afs_inode *afs_list_directory(const char *dirname, size_t *num_entries) {
+    // List directory implementation
+    return NULL;
+}
+
+int afs_get_metadata(const char *name, struct afs_inode *inode) {
+    // Get metadata implementation
+    return 0;
+}
+
+int afs_update_metadata(const char *name, const struct afs_inode *inode) {
+    // Update metadata implementation
+    return 0;
+}
