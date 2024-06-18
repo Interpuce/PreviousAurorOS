a4d583d1fbdb7459cfb5084136ecd96ba2a28316
Gorciu <gorciuyt@gmail.com>
2024-06-18 16:04:15 +0200

    Second-stage bootloader in C

---
+++ src/boot/bootloader.c
@@ -0,0 +1,10 @@
+// ======================================================================
+//                           AurorOS Bootloader
+// ======================================================================
+// => The AurorOS bootloader (C Stage).
+//       This file is basically a bootloader written in C.
+// ======================================================================
+
+void second_stage_entry() {
+    // ToDo: Make second stage bootloader that loads an kernel from the hard-drive
+}
