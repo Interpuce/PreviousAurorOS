53ca63faa469b6a2800e9669eac2b6a87f084bb3
Gorciu <gorciuyt@gmail.com>
2024-06-19 20:17:08 +0200

    Moved USB regs to USB header file

--- src/lib/usb.h
+++ src/lib/usb.h
@@ -2,7 +2,17 @@
 #define USB_DRIVER_H
 
 #include <stdbool.h>
-#include "usb_regs.h"
+#include <stdint.h>
+
+#define USB_BASE_ADDRESS 0x40080000
+#define USB_CTRL_REG     (*(volatile uint32_t *)(USB_BASE_ADDRESS + 0x00))
+#define USB_STATUS_REG   (*(volatile uint32_t *)(USB_BASE_ADDRESS + 0x04))
+#define USB_INT_ENABLE   (*(volatile uint32_t *)(USB_BASE_ADDRESS + 0x08))
+#define USB_INT_STATUS   (*(volatile uint32_t *)(USB_BASE_ADDRESS + 0x0C))
+#define USB_DATA_REG     (*(volatile uint32_t *)(USB_BASE_ADDRESS + 0x10))
+
+#define USB_CTRL_ENABLE  0x01
+#define USB_CTRL_RESET   0x02
 
 #define MAX_USB_DEVICES  256
 
