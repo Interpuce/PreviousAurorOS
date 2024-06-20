2627c3598ebc21cb3c97e5e1353e461b4c66d2c3
Gorciu <gorciuyt@gmail.com>
2024-06-20 21:04:21 +0200

    Bootloader changes

--- src/boot/bootloader.c
+++ src/boot/bootloader.c
@@ -5,6 +5,9 @@
 //       This file is basically a bootloader written in C.
 // ======================================================================
 
+// Load ARFS driver
+#include "../drivers/fs/arfs.h"
+
 void second_stage_entry() {
     // ToDo: Make second stage bootloader that loads an kernel from the hard-drive
 }
