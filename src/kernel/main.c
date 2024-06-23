424b97ea4b5cf048208fd3b70957f0f33e5c1483
Gorciu <gorciuyt@gmail.com>
2024-06-23 13:53:22 +0200

    The basic AurorOS kernel

---
+++ src/kernel/main.c
@@ -0,0 +1,14 @@
+// ========================================================
+//                      AurorOS Kernel
+// ========================================================
+
+// Loads kernel subsystems and initializes drivers.
+void init() {
+    // Actually it don't return anything
+}
+
+// The AurorOS main function.
+void main() {
+    // Init kernel
+    init();
+}
