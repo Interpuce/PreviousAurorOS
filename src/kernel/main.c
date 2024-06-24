93f42989c0ed64b31a04d27c2043b260d797fba0
Gorciu <gorciuyt@gmail.com>
2024-06-24 19:38:00 +0200

    Kernel placeholder

---
+++ src/kernel/main.c
@@ -0,0 +1,12 @@
+// ----------------------------- AurorOS -----------------------------
+//                       Created by Interpuce Team
+// -------------------------------------------------------------------
+
+// If you want to learn how kernel works, use documentation.
+// The code can be unreadable in some moments.
+
+
+// The main kernel function. Here kernel starts working and uses functions from the kernel API (in the future).
+int main() {
+    return 0; // Return 0 by default (success)
+}
