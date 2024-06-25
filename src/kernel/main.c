b77502da9566d20041a78ec4a9e1e30a1e88cd2b
Gorciu <gorciuyt@gmail.com>
2024-06-25 10:07:04 +0200

    Fixed path in kernel file

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -6,7 +6,7 @@
 // The code can be unreadable in some moments.
 
 // Include kernel API (from /includes/kernel.h)
-#include <kernel.h>
+#include <auror/kernel.h>
 
 // The main kernel function. Here kernel starts working and uses functions from the kernel API (in the future).
 int main() {
