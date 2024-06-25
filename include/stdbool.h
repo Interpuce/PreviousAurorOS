bddb00ef9cf4554eb17527ff7b2cf16a873d268a
Gorciu <gorciuyt@gmail.com>
2024-06-25 10:17:12 +0200

    Own definition of stdbool.h

---
+++ include/stdbool.h
@@ -0,0 +1,13 @@
+// ----------------------------- AurorOS -----------------------------
+//                       Created by Interpuce Team
+// -------------------------------------------------------------------
+
+// This is an substitute to default <stdbool.h> library file,
+// which was deleted by Makefile
+
+// Some files needs this file so it was rewritted 
+// to be helpful in these files.
+
+#define bool _Bool
+#define true 1
+#define false 0
