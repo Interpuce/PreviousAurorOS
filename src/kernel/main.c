31ab5f8b3d602c31f9c1574407b6d15d9243302c
Gorciu <gorciuyt@gmail.com>
2024-06-26 19:14:25 +0200

    Added basic console mode to the kernel

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -11,10 +11,16 @@
 // Include kernel API (from /includes/auror/kernel.h)
 #include <auror/kernel.h>
 
+// Include console driver
+#include "../drivers/console/run.h"
+
 // The main kernel function. Here kernel starts working and uses functions from the kernel API (in the future).
 int main() {
     // Init kernel subsystems
     init(1);
+
+    // Just for demostration print "Hello World!"
+    cm_init();
 
     // The main kernel loop
     while (true) {
