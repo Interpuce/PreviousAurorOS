e68e73642cdc6ce685b14c27f5490d4dbeb843be
Gorciu <gorciuyt@gmail.com>
2024-06-26 18:27:49 +0200

    A basic implementation of VGA console driver

---
+++ src/drivers/vga/console.h
@@ -0,0 +1,100 @@
+// ----------------------------- AurorOS -----------------------------
+//                       Created by Interpuce Team
+// -------------------------------------------------------------------
+
+// This is a basic VGA console driver. 
+
+// NOTE: For making it simple, many things are really To-DO.
+// We are going to make this driver really useful in the future.
+
+// Include the INT types.
+#include <stdint.h>
+
+// Video ports addresses
+#define VGA_CTRL_REG 0x3D4
+#define VGA_DATA_REG 0x3D5
+
+// Text mode screen dimensions
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+// Character color attributes
+#define VGA_COLOR_BLACK 0
+#define VGA_COLOR_WHITE 15
+
+// Global variables to track cursor position and attribute for primary monitor
+uint8_t vga_cursorx = 0;
+uint8_t vga_cursory = 0;
+uint8_t vga_cursorattr = VGA_COLOR_WHITE; // Default color
+
+// Function to set cursor position for primary monitor
+void vga_set_cursor_position(uint8_t x, uint8_t y) {
+    uint16_t position = y * VGA_WIDTH + x;
+
+    // Send high byte of the position to VGA controller
+    outb(VGA_CTRL_REG, 14);
+    outb(VGA_DATA_REG, position >> 8);
+    // Send low byte of the position to VGA controller
+    outb(VGA_CTRL_REG, 15);
+    outb(VGA_DATA_REG, position & 0xFF);
+
+    vga_cursorx = x;
+    vga_cursory = y;
+}
+
+// Function to set character attribute (color) for primary monitor
+void set_char_attribute(uint8_t attr) {
+    vga_cursorattr = attr;
+}
+
+// Function to display a character on the screen of primary monitor
+void vga_put_char(uint8_t c, uint8_t x, uint8_t y) {
+    volatile uint16_t *video_memory = (volatile uint16_t *)0xB8000;
+    uint16_t offset = y * VGA_WIDTH + x;
+    video_memory[offset] = (vga_cursorattr << 8) | c;
+}
+
+// Function to display a string on the screen of primary monitor
+void vga_print_string(const char *str) {
+    uint8_t x = vga_cursorx;
+    uint8_t y = vga_cursory;
+
+    while (*str != '\0') {
+        if (*str == '\n') {
+            x = 0;
+            y++;
+        } else {
+            vga_put_char(*str, x, y);
+            x++;
+            if (x >= VGA_WIDTH) {
+                x = 0;
+                y++;
+            }
+        }
+
+        if (y >= VGA_HEIGHT) {
+            // To-DO: Implement scrolling.
+            break;
+        }
+
+        str++;
+    }
+
+    vga_set_cursor_position(x, y);
+}
+
+// Function to read from console (stdin) for primary monitor
+char vga_read_console() {
+    // It will probably need a keyboard driver, so for making it simple just return X
+    char input = 'X';
+    return input;
+}
+
+// Function to clear the screen of primary monitor
+void vga_clear_screen() {
+    volatile uint16_t *video_memory = (volatile uint16_t *)0xB8000;
+    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; ++i) {
+        video_memory[i] = ' ' | (vga_cursorattr << 8);
+    }
+    vga_set_cursor_position(0, 0);
+}
