45beaf382615a175340c598e643ef2f8768d2df8
Gorciu <gorciuyt@gmail.com>
2024-06-26 20:10:31 +0200

    Added more comments to power.h

--- include/auror/power.h
+++ include/auror/power.h
@@ -3,3 +3,8 @@
 // -------------------------------------------------------------------
 
 // In the future it will do something.
+// It probably needs some Assembly, but I'm green at Assembly.
+
+// Let's write the placeholders instead.
+void shutdown() {}
+void restart() {}
