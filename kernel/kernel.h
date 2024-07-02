570642ee4d58a6f652863fd3b14583a17c9db5a2
Gorciu <gorciuyt@gmail.com>
2024-07-02 18:00:13 +0200

    Added more comments to the kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -11,19 +11,21 @@
 // The current AurorOS version in the major:minor:patch format
 #define OS_VERSION "0.0.1 Snapshot 1"
 
-// The success code.
+// The success code definition.
 #define CODE_SUCCESS 0
 
-// The invalid code
+// The invalid code definition.
 #define CODE_INVALID -1
 
 // Initializes kernel subsystems.
 int init(int start_code) {
     if (start_code == 1) {
         // Initializes kernel subsystems (in the future)
-        return CODE_SUCCESS; // Returns success
+        // For now AurorOS doesn't need any initialization
+
+        return CODE_SUCCESS; // Returns success.
     } else {
-        return CODE_INVALID; // The start code is invalid
+        return CODE_INVALID; // The start code is probably invalid or not implemented.
     }
 }
 
