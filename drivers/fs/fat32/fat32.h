36891ceb2c3232996ea5f5fb2425be2c20b2e6f5
Gorciu <gorciuyt@gmail.com>
2024-07-02 18:07:07 +0200

    The FAT32 structs

---
+++ drivers/fs/fat32/fat32.h
@@ -0,0 +1,57 @@
+// ----------------------------- AurorOS -----------------------------
+//                       Created by Interpuce Team
+// -------------------------------------------------------------------
+
+// This is a FAT32 filesystem driver.
+
+// NOTE: This driver needs more things, but for making simple OS
+//       and for now let implement the most important things.
+
+#include <stdint.h>
+
+struct BootSector {
+    uint8_t jmpBoot[3];
+    char OEMName[8];
+    uint16_t bytesPerSector;
+    uint8_t sectorsPerCluster;
+    uint16_t reservedSectors;
+    uint8_t numFATs;
+    uint16_t rootEntryCount;
+    uint16_t totalSectors16;
+    uint8_t media;
+    uint16_t FATSize16;
+    uint16_t sectorsPerTrack;
+    uint16_t numberOfHeads;
+    uint32_t hiddenSectors;
+    uint32_t totalSectors32;
+
+    uint32_t FATSize32;
+    uint16_t extFlags;
+    uint16_t FSVersion;
+    uint32_t rootCluster;
+    uint16_t FSInfo;
+    uint16_t backupBootSector;
+    uint8_t reserved[12];
+
+    uint8_t driveNumber;
+    uint8_t reserved1;
+    uint8_t bootSignature;
+    uint32_t volumeID;
+    char volumeLabel[11];
+    char fileSystemType[8];
+};
+
+struct DirectoryEntry {
+    char name[11];
+    uint8_t attr;
+    uint8_t ntRes;
+    uint8_t crtTimeTenth;
+    uint16_t crtTime;
+    uint16_t crtDate;
+    uint16_t lstAccDate;
+    uint16_t fstClusHI;
+    uint16_t wrtTime;
+    uint16_t wrtDate;
+    uint16_t fstClusLO;
+    uint32_t fileSize;
+};
