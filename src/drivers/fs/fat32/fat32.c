8eb24c735c23ea87f787f665b4a5fcaa9748da48
Gorciu <gorciuyt@gmail.com>
2024-07-03 10:37:23 +0200

    FAT32 C file

---
+++ src/drivers/fs/fat32/fat32.c
@@ -0,0 +1,11 @@
+// ----------------------------- AurorOS -----------------------------
+//                       Created by Interpuce Team
+// -------------------------------------------------------------------
+
+// This is a FAT32 filesystem driver.
+
+// NOTE: This driver needs more things, but for making simple OS
+//       and for now let implement the most important things.
+
+// Include FAT32 structures
+#include "fat32.h"
