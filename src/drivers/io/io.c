0bce0bfab468190d4d3b295e7091396aa877e638
Blackneeed <kontododatkowe.marcelclay@gmail.com>
2024-07-04 12:38:23 +0200

    Fix Makefile and some build errors, the rest is for other AurorOS developers

---
+++ src/drivers/io/io.c
@@ -0,0 +1,14 @@
+#include <io.h>
+#include <stdint.h>
+
+uint8_t inb(uint16_t port) 
+{
+    uint8_t val;
+    __asm__ volatile("inb %1, %0" : "=a"(val) : "Nd"(port));
+    return val;
+}
+
+void outb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
